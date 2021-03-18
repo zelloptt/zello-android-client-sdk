a7a463a495ec914bcf1cba2002501504d62e47e5
zello-eugene <eugene@zello.com>
2021-03-17 23:09:26 -0500

    Better doc Simplify media session management Updated aar

--- zello-sdk/src/main/java/com/zello/sdk/headset/HeadsetMediaSessionImpl.kt
+++ zello-sdk/src/main/java/com/zello/sdk/headset/HeadsetMediaSessionImpl.kt
@@ -5,8 +5,6 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.os.Handler
-import android.os.Looper
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import android.view.KeyEvent
@@ -19,7 +17,6 @@ class HeadsetMediaSessionImpl : HeadsetMediaSession {
 
 	private var mediaSession: MediaSessionCompat? = null
 	private var silencePlayer: SilencePlayer? = null
-	private var handler: Handler? = null
 	private var callback: MediaSessionCompat.Callback? = null
 
 	companion object {
@@ -30,25 +27,29 @@ class HeadsetMediaSessionImpl : HeadsetMediaSession {
 	 * Start the session.
 	 */
 	override fun start(context: Context, onKeyEvent: (KeyEvent) -> Unit) {
-		handler = Handler(Looper.getMainLooper())
 		HeadsetBroadcastReceiver.onKeyEvent = onKeyEvent
 		val mediaButtonReceiver = ComponentName(context.applicationContext, HeadsetBroadcastReceiver::class.java)
-		mediaSession = MediaSessionCompat(context.applicationContext, "zello sdk", mediaButtonReceiver, null).also { mediaSession ->
-			callback = object : MediaSessionCompat.Callback() {
-				override fun onMediaButtonEvent(mediaButtonEvent: Intent?): Boolean {
-					return (mediaButtonEvent != null && HeadsetBroadcastReceiver.handleIntent(mediaButtonEvent)) || super.onMediaButtonEvent(mediaButtonEvent)
-				}
+		callback = object : MediaSessionCompat.Callback() {
+			override fun onMediaButtonEvent(mediaButtonEvent: Intent?): Boolean {
+				return (mediaButtonEvent != null && HeadsetBroadcastReceiver.handleIntent(mediaButtonEvent)) || super.onMediaButtonEvent(mediaButtonEvent)
 			}
-
-			mediaSession.setCallback(callback)
-			val mediaButtonIntent = Intent(Intent.ACTION_MEDIA_BUTTON)
-			mediaButtonIntent.setClass(context.applicationContext, MediaButtonReceiver::class.java)
-			val pendingIntent = PendingIntent.getBroadcast(context.applicationContext, 0, mediaButtonIntent, 0)
-			mediaSession.setMediaButtonReceiver(pendingIntent)
-			mediaSession.isActive = true;
 		}
-		setMediaPlaybackState(PlaybackStateCompat.STATE_PLAYING)
-		createSilencePlayer(context)
+		mediaSession = MediaSessionCompat(context.applicationContext, "zello sdk", mediaButtonReceiver, null).also { session ->
+			session.setCallback(callback)
+			val mediaButtonIntent = Intent(Intent.ACTION_MEDIA_BUTTON).run {
+				setClass(context.applicationContext, MediaButtonReceiver::class.java)
+			}
+			session.setMediaButtonReceiver(PendingIntent.getBroadcast(context.applicationContext, 0, mediaButtonIntent, 0))
+			session.isActive = true;
+			// Switch the session to "playing" state
+			val playbackState = PlaybackStateCompat.Builder().run {
+				setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE or PlaybackStateCompat.ACTION_PAUSE)
+				setState(PlaybackStateCompat.STATE_PLAYING, PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN, 0f)
+			}.build()
+			session.setPlaybackState(playbackState)
+		}
+		// While not strictly necessary, playing a dummy sound may help with "stealing" an active media session from another app
+		startSilencePlayer(context)
 	}
 
 	/**
@@ -60,22 +61,10 @@ class HeadsetMediaSessionImpl : HeadsetMediaSession {
 		silencePlayer?.release()
 		silencePlayer = null
 		HeadsetBroadcastReceiver.onKeyEvent = null
-		handler = null
 		callback = null
 	}
 
-	private fun setMediaPlaybackState(state: Int) {
-		val builder = PlaybackStateCompat.Builder()
-		if (state == PlaybackStateCompat.STATE_PLAYING) {
-			builder.setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE or PlaybackStateCompat.ACTION_PAUSE)
-		} else {
-			builder.setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE or PlaybackStateCompat.ACTION_PLAY)
-		}
-		builder.setState(state, PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN, 0f)
-		mediaSession?.setPlaybackState(builder.build())
-	}
-
-	private fun createSilencePlayer(context: Context) {
+	private fun startSilencePlayer(context: Context) {
 		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
 		silencePlayer = SilencePlayerImpl21(context)
 	}
