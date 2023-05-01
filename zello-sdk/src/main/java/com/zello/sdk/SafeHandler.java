2f5ee728be82ccdca580a105d1e2c5b6cfa7f317
zello-eugene <eugene@zello.com>
2023-05-01 14:26:59 -0500

    wip

--- zello-sdk/src/main/java/com/zello/sdk/SafeHandler.java
+++ zello-sdk/src/main/java/com/zello/sdk/SafeHandler.java
@@ -8,10 +8,24 @@ import java.lang.ref.WeakReference;
 
 import androidx.annotation.NonNull;
 
+/**
+ * <p>
+ *     <code>Handler</code> wrapper that prevents a potential
+ *     memory leak occurring due to a raw <code>Handler</code>
+ *     being a member of a service or an activity.
+ * </p>
+ */
 public class SafeHandler<T extends SafeHandlerEvents> extends Handler {
 
 	private final @NonNull WeakReference<T> _t;
 
+	/**
+	 * <p>
+	 *     Primary constructor.
+	 * </p>
+	 * @param t Instance of {@link SafeHandlerEvents}
+	 * @param context Context
+	 */
 	public SafeHandler(@NonNull T t, @NonNull Context context) {
 		super(context.getMainLooper());
 		_t = new WeakReference<T>(t);
