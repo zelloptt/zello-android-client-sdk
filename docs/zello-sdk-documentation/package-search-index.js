95982dfc5f9f0b8f1f1e0ea2202602e71432aef3
zello-eugene <eugene@zello.com>
2023-05-02 13:39:23 -0500

    #86 Update documentation

--- docs/zello-sdk-documentation/package-search-index.js
+++ docs/zello-sdk-documentation/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.zello.sdk"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.zello.sdk"}];updateSearchResults();
